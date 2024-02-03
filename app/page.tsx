4d283cd82cea2dc9a826c9fcc4bf911f374ff85a
kapil.yadav <kapil.yadav@mclarensv.com>
2024-02-04 00:23:19 +0530

    Dummy buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,130 @@
-import Image from "next/image";
+"use client"; // This is a client component 👈🏽
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+<><><><><link rel="preconnect" href="https://fonts.googleapis.com" /><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /></><link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap" /></><link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons" /></><meta name="viewport" content="initial-scale=1, width=device-width" /></>
 
-export default function Home() {
+
+import {
+  createTransferCheckedInstruction,
+  getAssociatedTokenAddress,
+  getOrCreateAssociatedTokenAccount
+} from "@solana/spl-token";
+
+import {
+  Keypair,
+  Transaction,
+  Connection,
+  PublicKey,
+  TransactionInstruction,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
+import { PhantomProvider } from './interface';
+import { useState } from 'react';
+import { Button } from '@mui/material';
+
+export default  function main() { 
+  /**
+    * @description gets Phantom provider, if it exists
+  */
+  const getProvider = (): PhantomProvider | undefined => {
+    if ("solana" in window) {
+      // @ts-ignore
+      const provider = window.solana as any;
+      if (provider.isPhantom) return provider as PhantomProvider;
+    }
+  };
+
+
+  const [provider, setProvider] = useState<PhantomProvider | undefined>(undefined);
+  const [walletAddress, setWalletAddress] = useState<any>(undefined);
+  // const { publicKey, sendTransaction, signTransaction } = useWallet();
+
+  const connectWallet = async () => {
+    const provider = getProvider();
+    if (provider) { 
+      setProvider(provider);
+      try {
+        const connStatus = await provider.connect();
+        const pubKey = connStatus?.publicKey.toString()
+        setWalletAddress(pubKey);
+      } catch(error) {
+        console.error("Error connecting to wallet:", error);
+      }
+    }
+    else {
+      console.warn("Phantom wallet provider not found.");
+      setProvider(undefined);
+    }
+  }
+
+  const disconnectWallet = async () => {
+    provider?.disconnect();
+    setProvider(undefined);
+    setWalletAddress(undefined);
+  }
+
+  const tokenTransfer = async () => {
+    const TOKEN_DECIMAL = 9;
+    const USDCAddress = '41fMjoW1G7uYUE5283cMmGVPMat2yx4EmEMZEVaTFUhm'
+
+    const connection = new Connection(
+      'https://solana-devnet.g.alchemy.com/v2/2Om9vVrMNswSL0zrm_xLCUnLqJbqKYtW',
+       "confirmed"
+   );
+   
+   const recipient = 'CnJwPXgr8LkywNbYiRBPpd9ZPsooawcyh98CAKw1KSPK';
+
+   const senderTokenAccountAddress = await getAssociatedTokenAddress(
+    new PublicKey(USDCAddress),
+    new PublicKey(walletAddress)
+   )
+   console.log("senderTokenAccountAddress::::", senderTokenAccountAddress.toString());
+
+   const recipientTokenAccountAddress = await getAssociatedTokenAddress(
+    new PublicKey(USDCAddress),
+    new PublicKey(recipient)
+    )
+    console.log("RecepientTokenAccountAddress:::", recipientTokenAccountAddress.toString());
+
+    const instruction = createTransferCheckedInstruction(
+      senderTokenAccountAddress,
+      new PublicKey(USDCAddress),
+      recipientTokenAccountAddress,
+      new PublicKey(walletAddress),
+      1 ** 9,
+      TOKEN_DECIMAL, // Replace with the decimals of the token
+    );
+    console.log("instruction:::", instruction);
+
+    const transaction = new Transaction().add(instruction);
+
+    const txHash = await provider.SignAndSendTransaction(transaction);
+    // const transactionSignature3 = await sendAndConfirmTransaction(
+    //   connection,
+    //   transaction,
+    //   [provider] // signer
+    // );
+  }
+
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    <div className="App">
+      <header className="App-header">
+          {/* Display wallet address if available */}
+          {walletAddress ? (<p>Connected Wallet Address: {walletAddress}</p>) : <p>Currently not connected</p> }
+        </header>
+        {/* Display token instruction data to UI */}
+      <Button variant="outlined"  onClick={connectWallet}>Connect Wallet</Button> <br></br>
+      <Button variant="outlined"  onClick={disconnectWallet}>Disconnect Wallet</Button>
+      <Button variant="outlined"  onClick={tokenTransfer} disabled={!provider || !walletAddress}>Transfer One token</Button> <br></br>
+    </div>
+  )
 }
